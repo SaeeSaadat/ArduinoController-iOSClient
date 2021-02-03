560d29ddcdf56256e9123b17526d0dffaea9b735
saee <saee_saadat@outlook.com>
2021-02-03 20:11:13 +0330

    d

--- AurdinoController-ios/SceneDelegate.swift
+++ AurdinoController-ios/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        //just a comment
+        //just a comment 2
         if let appdel = UIApplication.shared.delegate as? AppDelegate {
             appdel.initials(window: window!)
         }
